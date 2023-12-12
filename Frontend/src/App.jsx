6144325a3bc76cd6eaad1a774564d9669d579870
A.Tommy <tommy.angibaud@gmail.com>
2023-12-12 18:28:08 +0100

    Initial commit

---
+++ Frontend/src/App.jsx
@@ -0,0 +1,16 @@
+import React from "react"
+import Router from "./02.navigation/Router"
+
+function App() {
+
+
+  return (
+    <div>
+   
+      <Router />
+
+    </div>
+  )
+}
+
+export default App
