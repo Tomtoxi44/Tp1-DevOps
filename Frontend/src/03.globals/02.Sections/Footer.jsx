c156747f56fe8ce440e578cc2bed11805aacadd2
J-E <beillevertj.contact@gmail.com>
2024-01-08 12:30:17 +0100

    stylize footer

--- Frontend/src/03.globals/02.Sections/Footer.jsx
+++ Frontend/src/03.globals/02.Sections/Footer.jsx
@@ -2,8 +2,8 @@ import React from 'react';
 
 const Footer = () => {
     return (
-        <footer className='bg-neutral-800 w-full h-6'>
-            
+        <footer className='bg-neutral-800 w-full'>
+            <p className='text-neutral-500 text-center text-sm py-2 tracking-tight'>@Toxic corporation, all rights reserved, 2024</p>
         </footer>
     );
 };
