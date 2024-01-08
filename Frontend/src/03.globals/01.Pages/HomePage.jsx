ff9b04a2770c38f4c428162d7037c6603a1f7ea3
Tommy <tommy.angibaud@gmail.com>
2024-01-08 12:23:24 +0100

    Ajout d'une navbar

--- Frontend/src/03.globals/01.Pages/HomePage.jsx
+++ Frontend/src/03.globals/01.Pages/HomePage.jsx
@@ -1,9 +1,10 @@
 import React from 'react';
+import Navbar from '../03.Components/Navbar';
 
 const HomePage = () => {
     return (
         <div>
-            <p>hello</p>
+            <Navbar></Navbar>
         </div>
     );
 };
